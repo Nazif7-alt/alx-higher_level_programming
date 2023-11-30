d1abf635200e0f20fc3b8021d886a35d2f173b65
Nazif7-alt <arounanazif52@gmail.com>
2023-11-30 14:02:38 -0800

    task0 to

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,6 @@
 #importation of modul
 from add_0 import add
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
