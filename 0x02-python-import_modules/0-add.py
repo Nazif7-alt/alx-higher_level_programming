920aa330a21dc9b494e94f0cca8d68b90d60a215
Nazif7-alt <arounanazif52@gmail.com>
2023-11-30 15:09:31 -0800

    So i try again

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """thre rel program"""
     from add_0 import add
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
 
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
