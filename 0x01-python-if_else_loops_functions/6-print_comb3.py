26437e0afd955aa03d3d256c53124fe79bd5cab9
Nazif7-alt <arounanazif52@gmail.com>
2023-11-29 18:29:54 -0800

    task6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for number1 in range(9):
+    for number2 in range(i + 1, 10):
+        if number1 * 10 + number2 < 89:
+            print("{:d}{:d}".format(number1, number2), end=", ")
+print("{:d}".format(89))
