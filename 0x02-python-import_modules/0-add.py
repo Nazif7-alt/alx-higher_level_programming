fc6e1fa2ae29351be0dd52b0e24f9aa188cdf293
Nazif7-alt <arounanazif52@gmail.com>
2023-11-30 14:46:18 -0800

    task0 commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-#importation of modul
+
 if __name__ == "__main__":
+    """thre rel program"""
     from add_0 import add
-    #Now the declaration
+
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
+
