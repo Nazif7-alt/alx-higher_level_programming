ee3e814ebe3631683d17c39401db9911aa7e4a3b
Nazif7-alt <arounanazif52@gmail.com>
2023-11-30 15:23:40 -0800

    an other commit task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    # now lets show  sorted 
+    # Print sorted name from directory
     for name in sorted(dir(hidden_4)):
-        #print it in nice format
+        # print only names that do not start with __
         if name[:2] != '__':
             print("{}".format(name))
