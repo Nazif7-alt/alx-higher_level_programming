2adfdeed292f355b6642fd5bdc782b3d76116661
Nazif7-alt <arounanazif52@gmail.com>
2023-12-04 15:19:48 -0800

    Task0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#Lets declare my funcion like task ask
+def print_list_integer(my_list=[]):
+    #lets use boucle for to visit all contain on my list
+    for i in my_list:
+        print("{:d}".format(i))
