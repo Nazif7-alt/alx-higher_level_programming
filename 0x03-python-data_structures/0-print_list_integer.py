713b79d7490f61679b8398ec0b9dd317f7c55fb3
Nazif7-alt <arounanazif52@gmail.com>
2023-12-04 15:46:50 -0800

    Task0 again3 with chmod

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+#Lets define ;y function
 def print_list_integer(my_list=[]
+    #This for condition can run all list contains
     for i in my_list:
     print("{:d}".format(i))
